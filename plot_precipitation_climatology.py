da85c1fd3a6b082788c51964c3b84744c72f826e
Maxime Marin <maxime.marin@utas.edu.au>
2021-04-11 15:35:50 +0800

    lols

--- plot_precipitation_climatology.py
+++ plot_precipitation_climatology.py
@@ -1,4 +1,5 @@
 import argparse
+
 import xarray as xr
 import cartopy.crs as ccrs
 import matplotlib.pyplot as plt
@@ -6,10 +7,9 @@ import numpy as np
 import cmocean
 
 
-
 def convert_pr_units(darray):
     """Convert kg m-2 s-1 to mm day-1.
-    
+   
     Args:
       darray (xarray.DataArray): Precipitation data
     
@@ -17,27 +17,41 @@ def convert_pr_units(darray):
     
     darray.data = darray.data * 86400
     darray.attrs['units'] = 'mm/day'
-    
+   
     return darray
 
-
-def create_plot(clim, model, season, gridlines=False):
-    """Plot the precipitation climatology.
+def apply_mask(clim,file,realm):
     
+    dset = xr.open_dataset(file)
+    sftlf = dset['sftlf']
+    if realm == 'land':
+        clim_ocean = clim.where(sftlf<50)
+    else:
+        clim_ocean = clim.where(sftlf>50)
+            
+    return clim_ocean
+
+def create_plot(clim, model_name, season, gridlines=False, levels=None):
+    """Plot the precipitation climatology.
+   
     Args:
       clim (xarray.DataArray): Precipitation climatology data
-      model (str): Name of the climate model
+      model_name (str): Name of the climate model
       season (str): Season
       
     Kwargs:
-      gridlines (bool): Select whether to plot gridlines    
+      gridlines (bool): Select whether to plot gridlines
+      levels (list): Tick marks on the colorbar    
     
     """
+
+    if not levels:
+        levels = np.arange(0, 13.5, 1.5)
         
     fig = plt.figure(figsize=[12,5])
     ax = fig.add_subplot(111, projection=ccrs.PlateCarree(central_longitude=180))
     clim.sel(season=season).plot.contourf(ax=ax,
-                                          levels=np.arange(0, 13.5, 1.5),
+                                          levels=levels,
                                           extend='max',
                                           transform=ccrs.PlateCarree(),
                                           cbar_kwargs={'label': clim.units},
@@ -46,7 +60,7 @@ def create_plot(clim, model, season, gridlines=False):
     if gridlines:
         plt.gca().gridlines()
     
-    title = f'{model} precipitation climatology ({season})'
+    title = '%s precipitation climatology (%s)' %(model_name, season)
     plt.title(title)
 
 
@@ -55,22 +69,33 @@ def main(inargs):
 
     dset = xr.open_dataset(inargs.pr_file)
     
-    clim = dset['pr'].groupby('time.season').mean('time', keep_attrs=True)
+    clim = dset['pr'].groupby('time.season').mean('time')
     clim = convert_pr_units(clim)
+    if inargs.mask:
+        file,realm = inargs.mask
+        clim = apply_mask(clim,file,realm)
 
-    create_plot(clim, dset.attrs['source_id'], inargs.season)
+    create_plot(clim, dset.attrs['source_id'], inargs.season,
+                gridlines=inargs.gridlines, levels=inargs.cbar_levels)
     plt.savefig(inargs.output_file, dpi=200)
 
 
 if __name__ == '__main__':
-    description='Plot the precipitation climatology for seasons shit.'
+    description='Plot the precipitation climatology for a given season.'
     parser = argparse.ArgumentParser(description=description)
-    
+   
     parser.add_argument("pr_file", type=str, help="Precipitation data file")
     parser.add_argument("season", type=str, help="Season to plot")
     parser.add_argument("output_file", type=str, help="Output file name")
 
+    parser.add_argument("--gridlines", action="store_true", default=False,
+                        help="Include gridlines on the plot")
+    parser.add_argument("--cbar_levels", type=float, nargs='*', default=None,
+                        help='list of levels / tick marks to appear on the colorbar')
+    parser.add_argument("--mask", type=str, nargs=2,
+                    metavar=('SFTLF_FILE', 'REALM'), default=None,
+                    help="""Provide sftlf file and realm to mask ('land' or 'ocean')""")
+
     args = parser.parse_args()
-    
+   
     main(args)
-    
